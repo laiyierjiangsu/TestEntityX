90411e24c149b365dd0a3764b5b92a373dcd7426
laiyierjiangsu <laiyierjiangsu@gmail.com>
2017-07-14 15:49:14 +0800

    Add some test code

---
+++ entityx/entityx/config.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <cstdint>
+#include <cstddef>
+
+namespace entityx {
+
+static const size_t MAX_COMPONENTS = 65535/*ENTITYX_MAX_COMPONENTS*/;
+typedef int TimeDelta;
+
+}  // namespace entityx
